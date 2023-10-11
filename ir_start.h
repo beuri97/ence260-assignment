9cab421f01f1997fe2c3c6b5b6e8bbb8004c87bc
Blake Manson <bma206@cs23163fw.canterbury.ac.nz>
2023-10-11 16:47:43 +1300

    typo

--- ir_start.h
+++ ir_start.h
@@ -3,6 +3,6 @@
 #include <stdbool.h>
 
 void sendDone(char order);
-uint8_t reveiveDone(void);
+uint8_t receiveDone(void);
 uint8_t getOrder(void);
-void ir_start_init(bool finished);
+void ir_start_init(void);
