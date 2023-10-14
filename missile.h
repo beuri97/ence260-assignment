4ae6eaa0c90031ba03b76aff87b8fceddf5d43b9
Blake Manson <bma206@cs23135fw.canterbury.ac.nz>
2023-10-14 15:45:05 +1300

    added missile

---
+++ missile.h
@@ -0,0 +1,12 @@
+#ifndef MISSILE_H
+#define MISSILE_H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+void missile_init(uint16_t rate);
+
+#endif //MISSILE_H
