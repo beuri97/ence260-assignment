af3afc62a81e819cc5864452639d370f9b9ec91c
Blake Manson <bma206@cs23135fw.canterbury.ac.nz>
2023-10-14 15:42:47 +1300

    added turn order and missile

--- game.c
+++ game.c
@@ -3,7 +3,9 @@
 #include "pacer.h"
 #include "led.h"
 #include "tinygl.h"
+#include "ir_start.h"
 #include "../fonts/font5x7_1.h"
+#include "missile.h" // change
 
 #define PACER_RATE 500
 #define MESSAGE_RATE 10
@@ -21,7 +23,9 @@ int main (void)
     tinygl_draw_message("Choose ship positions", tinygl_point (0, 0), 1);
     pacer_init(PACER_RATE);
     ship_init(PACER_RATE);
+    uint8_t position = ir_start_init();
     led_set(LED1, 0);
+    missile_init(PACER_RATE);
     while (1)
     {
         
