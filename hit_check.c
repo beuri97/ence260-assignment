12e2133bb64a3d24e6774de916b4ba465181ad06
Blake Manson <bma206@cs23135fw.canterbury.ac.nz>
2023-10-14 17:29:26 +1300

    checking for hit

---
+++ hit_check.c
@@ -0,0 +1,13 @@
+#include "hit_check.h"
+#include "object.h"
+#include "display.h"
+#include "battleship.h"
+
+bool check_collision(uint8_t col, uint8_t row)
+{
+    bool hit = false;
+
+    
+
+    return hit;
+}
---
+++ hit_check.h
@@ -0,0 +1,10 @@
+#ifndef HIT_CHECK.H
+#define HIT_CHECK.H
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+#endif //HIT_CHECK.H
